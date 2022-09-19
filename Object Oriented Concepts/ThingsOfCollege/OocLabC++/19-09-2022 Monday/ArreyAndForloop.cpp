e563acb64a753d155c6657526615285f1a948dea
Bickeykr <79741948+Bickeykr@users.noreply.github.com>
2022-09-19 11:55:08 +0530

    arreyWithForLoop

---
+++ Object Oriented Concepts/ThingsOfCollege/OocLabC++/19-09-2022 Monday/ArreyAndForloop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int x[10];
+	for (int i=0; i<10; i=i+1){
+		
+	cout<<"Enter data\n";
+ 	cin>>x[i];
+	}
+	
+	for (int i=0; i<10; i=i+1){
+		
+	cout<<x[i]<<" ";	
+	}
+	return 0;
+
+	}
