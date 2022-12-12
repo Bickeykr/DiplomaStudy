5ad2b54943ef3e79b789f3a266895aa617dd111c
Bickey Kumar <79741948+Bickeykr@users.noreply.github.com>
2022-12-12 05:58:08 +0000

    12-12-2022 commit

--- C programming/C programming lab/02-09-2022 Friday (DefiningVariableAndAssigning, Experiment 1)/DefiningVariableAndAssigningValuesToVariable.c
+++ C programming/C programming lab/02-09-2022 Friday (DefiningVariableAndAssigning, Experiment 1)/DefiningVariableAndAssigningValuesToVariable.c
@@ -3,6 +3,6 @@
 void main(){
  	int a=29, b=56;
 	char aa='a';
-	printf("Hello world, the value of a and b is %d,%d",a ,b);
+	printf("Hello world,My name is bickey and the value of a and b is %d,%d",a ,b);
 
 }
---
+++ Object Oriented Concepts/OocLabC++/Experiment 2 (Array, forLoop, String)/12-12-2022 Monday (pointer)/pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+using namespace std;
+
+void main(){
+    int a, *ptr1, **ptr2;
+     ptr1= &a;
+    ptr2= &ptr1;
+    cout<<"The address of a:"<<ptr1<<"\n";
+    cout<<"The address of ptr1:"<<ptr2;
+    cout<<"\n\n";
+    cout<<"After incrementing the address values : \n\n";
+    ptr1+=2;
+    cout<<"The address of a :"<<ptr1<<"\n";
+    ptr2+=2;
+    cout<<"The address of ptr1 :"<<ptr2<<"\n";
+}
